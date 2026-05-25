e64a75dbf47d42e9ff4064454cded5e8e8a2f245
Yash <yashgaikwad3295@gmail.com>
2026-05-25 20:19:40 +0530

    leetcode 73

--- Array Part3/set_matrix_zeros.cpp
+++ Array Part3/set_matrix_zeros.cpp
@@ -4,29 +4,27 @@
 #include<vector>
 using namespace std;
 
-vector<vector<int>> set_matrix_zeroes(vector<vector<int>> &matrix) {
+vector<vector<int>> set_matrix_zeroes(vector<vector<int>> matrix) {
     int n = matrix.size();
     int m = matrix[0].size();
-
     int col0 = 1;
-    
+
     for(int i=0; i<n; i++) {
         for(int j=0; j<m; j++) {
             if(matrix[i][j] == 0) {
                 matrix[i][0] = 0;
 
                 if(j != 0) {
-                matrix[0][j] = 0;
-            }else {
-                col0 = 0;
+                    matrix[0][j] = 0;
+                }else {
+                    col0 = 0;
+                }
             }
-            }
-
         }
     }
-
-    for(int i=1; i<n; i++) {
-        for(int j=1; j<m; j++) {
+    
+    for(int i = 1; i<n; i++){
+        for(int j=1; j<n; j++) {
             if(matrix[i][j] != 0) {
                 if(matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
@@ -36,10 +34,10 @@ vector<vector<int>> set_matrix_zeroes(vector<vector<int>> &matrix) {
     }
 
     if(matrix[0][0] == 0) {
-        for(int j=0; j<m; j++) {
+        for(int j=0; j<m; j++){
             matrix[0][j] = 0;
         }
-    } 
+    }
 
     if(col0 == 0) {
         for(int i=0; i<n; i++) {
@@ -56,12 +54,14 @@ int main() {
                                 {2,1,6}
 };
 
-     vector<vector<int>>result  = set_matrix_zeroes(matrix);        
+    vector<vector<int>>result  = set_matrix_zeroes(matrix);   
      
-    for(auto row : result) {
+     for(auto row : result) {
         for(int val : row) {
             cout << val << " ";
         }
         cout << endl;
-    }
+     }
+     
+    
 }
